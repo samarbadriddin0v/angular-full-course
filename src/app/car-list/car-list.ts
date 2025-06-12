6475f02eef1ca006a16d25a5033f9e3e9dd1d013
samarbadriddin0v <sammibadriddinov@gmail.com>
2025-06-12 09:51:45 +0300

    Fetching list

--- src/app/car-list/car-list.ts
+++ src/app/car-list/car-list.ts
@@ -1,17 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CarService } from '../serviecs/car.service';
+import { Reservation } from '../models/reservation';
+import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-car-list',
-  imports: [],
+  imports: [DatePipe, RouterLink],
   templateUrl: './car-list.html',
   styleUrl: './car-list.css',
   standalone: true,
 })
-export class CarList {
+export class CarList implements OnInit {
+  reservationList: Reservation[] = [];
   reservetionService = inject(CarService);
 
-  constructor() {
-    console.log(this.reservetionService.getReservations());
+  ngOnInit(): void {
+    this.reservationList = this.reservetionService.getReservations();
+  }
+
+  deleteReservation(id: number): void {
+    this.reservetionService.deleteReservation(id);
+    this.reservationList = this.reservetionService.getReservations();
   }
 }
