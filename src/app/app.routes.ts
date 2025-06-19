189709a409deb5c7897c0009a0913059b5e98f8b
samarbadriddin0v <sammibadriddinov@gmail.com>
2025-06-19 12:44:23 +0300

    Understanding environments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { CarList } from './car-list/car-list';
-import { CarForm } from './car-form/car-form';
+import { ProductList } from './product-list/product-list';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'list', component: CarList },
-  { path: 'new', component: CarForm },
-  { path: 'edit/:id', component: CarForm },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductList },
 ];
--- src/app/models/reservation.ts
+++
@@ -1,9 +0,0 @@
-export interface Reservation {
-  id: string;
-  checkIn: Date;
-  checkOut: Date;
-  clientName: string;
-  clientEmail: string;
-  carModel: string;
-  carNumber: number;
-}
