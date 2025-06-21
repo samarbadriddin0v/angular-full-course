70cac29067c26bc693f365fbd9306dd4b3bc266e
samarbadriddin0v <sammibadriddinov@gmail.com>
2025-06-21 05:32:43 +0300

    Understanding signal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { ProductList } from './product-list/product-list';
 
-export const routes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', component: ProductList },
-];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [RouterOutlet, RouterLink],
+  imports: [],
 })
 export class App {
-  protected title = 'angular-course';
+  count = signal(0);
+
+  increment() {
+    this.count.update((value) => value + 1);
+  }
+
+  decrement() {
+    this.count.update((value) => value - 1);
+  }
+
+  reset() {
+    this.count.set(0);
+  }
 }
