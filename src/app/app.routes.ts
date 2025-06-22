4b7220a232a480473f635e3d73fee75b27e698ef
samarbadriddin0v <sammibadriddinov@gmail.com>
2025-06-22 17:23:06 +0300

    Cart service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductList } from './product-list/product-list';
+import { ShoppingCart } from './shopping-cart/shopping-cart';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: 'products', component: ProductList },
+  { path: 'cart', component: ShoppingCart },
 ];
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Cart } from '../models/cart';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  private http = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
+
+  getCart(): Observable<Cart[]> {
+    return this.http.get<Cart[]>(`${this.apiUrl}/cart`);
+  }
+
+  addToCart(cart: Cart): Observable<Cart> {
+    return this.http.post<Cart>(`${this.apiUrl}/cart`, cart);
+  }
+
+  removeFromCart(cartId: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/cart/${cartId}`);
+  }
+
+  updateQunatity(cartId: string, quantity: number): Observable<Cart> {
+    return this.http.patch<Cart>(`${this.apiUrl}/cart/${cartId}`, { quantity });
+  }
+
+  clearCart(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/cart`);
+  }
+}
